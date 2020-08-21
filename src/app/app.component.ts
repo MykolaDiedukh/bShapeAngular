d4b3aca818fcf79b08feb57dea74113d7756d277
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-08-21 12:25:52 +0200

    Added Validation to Login and registration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,11 @@ import {HttpClient}  from '@angular/common/http'
 export class AppComponent {
   constructor(private http: HttpClient) {
   }
+  errors=[{
+    show:false,
+    description:"hasła nie są takie same"
+
+  }]
 
   loginForm = new FormGroup({username :new FormControl(), password : new FormControl()}) // grupa formularza Form Control tworzy pola w formularzu.
   LoginUser = (form)=>{
@@ -20,4 +25,21 @@ export class AppComponent {
     //   })
   }
 
+  registerForm = new FormGroup({firstName: new FormControl(), lastName: new FormControl(), email : new FormControl(), sex : new FormControl(), password : new FormControl(), submitPassword : new FormControl() })
+
+
+  RegisterUser = (form)=> {
+    console.log(form)
+
+    if (form.password === form.submitPassword) {
+      // this.http.get("/adrresAPI")
+      this.errors[0].show = false;
+      //   .subscribe((response)=>{
+      //     // robie cos z responsem z bazy danych
+      //   })
+    } else {
+      this.errors[0].show = true;
+
+    }
+  }
 }
