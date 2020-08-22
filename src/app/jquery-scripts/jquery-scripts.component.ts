b6ffffdbf16f57e15c3b3c4540bfc3e0ee3fb1d8
Krystian-Key <kluczkiewicz1512@wp.pl>
2020-08-22 11:17:55 +0200

    still to cofig

---
+++ src/app/jquery-scripts/jquery-scripts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jquery-scripts',
+  templateUrl: './jquery-scripts.component.html',
+  styleUrls: ['./jquery-scripts.component.css']
+})
+export class JQueryScriptsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
