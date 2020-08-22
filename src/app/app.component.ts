18e3c187a5d061c339223193183d9ca082c2bcde
maksymilianblok <maksymilian.blok@upbonus.pl>
2020-08-22 11:58:58 +0200

    fixed show password button + imported jquery + imported lightbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {HttpClient}  from '@angular/common/http'
+import * as $ from 'jquery';
+import * as lightbox from 'lightbox2'
 
 @Component({
   selector: 'app-root',
@@ -10,14 +12,15 @@ import {HttpClient}  from '@angular/common/http'
 export class AppComponent {
   constructor(private http: HttpClient) {
   }
-  errors=[{
-    show:false,
-    description:"hasła nie są takie same"
+
+  errors = [{
+    show: false,
+    description: "hasła nie są takie same"
 
   }]
 
-  loginForm = new FormGroup({username :new FormControl(), password : new FormControl()}) // grupa formularza Form Control tworzy pola w formularzu.
-  LoginUser = (form)=>{
+  loginForm = new FormGroup({username: new FormControl(), password: new FormControl()}) // grupa formularza Form Control tworzy pola w formularzu.
+  LoginUser = (form) => {
     console.log(form)
     // this.http.get("/adrresAPI")
     //   .subscribe((response)=>{
@@ -25,10 +28,17 @@ export class AppComponent {
     //   })
   }
 
-  registerForm = new FormGroup({firstName: new FormControl(), lastName: new FormControl(), email : new FormControl(), sex : new FormControl(), password : new FormControl(), submitPassword : new FormControl() })
+  registerForm = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    email: new FormControl(),
+    sex: new FormControl(),
+    password: new FormControl(),
+    submitPassword: new FormControl()
+  })
 
 
-  RegisterUser = (form)=> {
+  RegisterUser = (form) => {
     console.log(form)
 
     if (form.password === form.submitPassword) {
@@ -42,4 +52,34 @@ export class AppComponent {
 
     }
   }
+
+  public ngOnInit() {
+    // @ts-ignore
+    !function ($) {
+//eyeOpenClass: 'fa-eye',
+//eyeCloseClass: 'fa-eye-slash',
+      'use strict';
+
+      $(function () {
+        $('[data-toggle="password"]').each(function () {
+          var input = $(this);
+          var eye_btn = $(this).parent().find('.input-group-text');
+          eye_btn.css('cursor', 'pointer').addClass('input-password-hide');
+          eye_btn.on('click', function () {
+            if (eye_btn.hasClass('input-password-hide')) {
+              eye_btn.removeClass('input-password-hide').addClass('input-password-show');
+              eye_btn.find('.fa').removeClass('fa-eye').addClass('fa-eye-slash')
+              input.attr('type', 'text');
+            } else {
+              eye_btn.removeClass('input-password-show').addClass('input-password-hide');
+              eye_btn.find('.fa').removeClass('fa-eye-slash').addClass('fa-eye')
+              input.attr('type', 'password');
+            }
+          });
+        });
+      });
+      // @ts-ignore
+    }(window.jQuery);
+  }
+
 }
