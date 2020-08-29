c2e97b23f0b9f46b3ebf344b37e68eb096346843
soul <ykrainec1991@gmail.com>
2020-08-29 17:50:08 +0200

    update json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
-import {HttpClient}  from '@angular/common/http'
-import * as lightbox from 'lightbox2'
+import {HttpClient} from '@angular/common/http';
+import * as lightbox from 'lightbox2';
 declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   constructor(private http: HttpClient) {
   }
 
